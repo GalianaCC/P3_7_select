a92147682620b5153373ef32df3d23002ae02514
GalianaCC <galianacc@easdalcoi.es>
2026-01-08 09:53:14 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,9 @@
+var selectcolor = document.getElementById("select_color");
+function setup() {
+  var c= createCanvas(400, 400);
+  c.parent("canvasWrapper");
+}
+
+function draw() {
+  background(selectcolor.value);
+}
